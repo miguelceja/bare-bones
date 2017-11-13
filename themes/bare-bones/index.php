44cf866545952a77ecd75de39b08f6a862727678
Miguel Ceja <miguelceja222@gmail.com>
2017-11-13 13:55:29 -0800

    Moving Grunt to wp-content

---
+++ themes/bare-bones/index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template for the index template.
+ *
+ * @package BareBones
+ * @author  Miguel Ceja <https://miguelceja.com>
+ * @link    https://codex.wordpress.org/Template_Hierarchy
+ */
+
+get_header();
+
+?>
+
+	<div>
+		<p>This is the index.php</p>
+
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+			<h2><?php the_title(); ?></h2>
+			<?php the_content(); ?>
+
+		<?php endwhile; else : ?>
+
+			<p>There are no posts or pages here.</p>
+
+		<?php endif; ?>
+	</div>
+
+<?php get_footer(); ?>
