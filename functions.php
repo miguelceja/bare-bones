93f88a2024ebd35516e5a4a8739d4ad6d655dab7
Miguel Ceja <miguelceja222@gmail.com>
2017-11-13 10:11:02 -0800

    Adding more descriptive comments to functions

--- functions.php
+++ functions.php
@@ -1,39 +1,46 @@
 <?php
+/**
+ * Theme functions and definitions.
+ *
+ * @package BareBones
+ * @author  Miguel Ceja <https://miguelceja.com>
+ * @link    https://developer.wordpress.org/themes/basics/theme-functions/
+ */
 
-// Load the theme css
-
-function theme_styles() {
-
-// If you want to link up other css files in a css directory, just copy the wp_enqueue line below and put in the file name instead of 'main' and add /css to the location at the end of hte line.
-// To link up an external sheet, like google web fonts...
-// wp_enqueue_style( 'googlewebfonts', 'http://fonts.googleapis.com/css?family=Chango' );
+ /**
+  * Load the theme CSS
+  */
+function bb_theme_styles() {
 
 	wp_enqueue_style( 'main', get_template_directory_uri() . '/css/build/main.css' );
-
-	// Import fonts, such as Google fonts
-	//wp_enqueue_style( 'googlewebfonts-Oxygen', 'http://fonts.googleapis.com/css?family=Oxygen:400,300,700' );
-
+	wp_enqueue_style( 'googlewebfonts-Oxygen', 'http://fonts.googleapis.com/css?family=Oxygen:400,300,700' );
 }
 
-add_action( 'wp_enqueue_scripts', 'theme_styles' );
+add_action( 'wp_enqueue_scripts', 'bb_theme_styles' );
 
-// Load the theme js
-function theme_js() {
-
-	// Load the theme.js file
-	wp_register_script( 'theme_js', get_template_directory_uri() . '/js/build/app.js', array('jquery'), '', true );
-	wp_enqueue_script( 'theme_js');
+/**
+ * Load the theme JS.
+ */
+function bb_theme_js() {
+	wp_register_script( 'bb_theme_js', get_template_directory_uri() . '/js/build/app.js', array( 'jquery'), '', true );
+	wp_enqueue_script( 'bb_theme_js' );
 }
 
-add_action( 'wp_enqueue_scripts', 'theme_js');
+add_action( 'wp_enqueue_scripts', 'bb_theme_js' );
 
-
-// Enable custom menus
+/**
+ * Add custom menu spport.
+ */
 add_theme_support( 'menus' );
 
-
-// Enable widgets
-function create_widget( $name, $id, $description) {
+/**
+ * Below are examples of possible widgets for the footer, but they can be customized and moved anywhere.
+ *
+ * @param string $name Name of footer widget.
+ * @param string $id ID given to widget.
+ * @param string $description where the widget is displayed in the footer.
+ */
+function bb_create_widget( $name, $id, $description ) {
 	$args = array(
 		'name'          => __( $name ),
 		'id'            => $id,
@@ -41,15 +48,12 @@ function create_widget( $name, $id, $description) {
 		'before_widget' => '',
 		'after_widget'  => '',
 		'before_title'  => '<h5>',
-		'after_title'   => '</h5>'
+		'after_title'   => '</h5>',
 	);
 
 	register_sidebar( $args );
 }
 
-// Enable footer widgets
-create_widget( 'Left Footer', 'footer_left', 'Displays in the bottom left of footer' );
-create_widget( 'Middle Footer', 'footer_middle', 'Displays in the middle of the footer' );
-create_widget( 'Right Footer', 'footer_right', 'Displays in the bottom right of the footer' );
-
-?>
+bb_create_widget( 'Left Footer', 'footer_left', 'Displays in the bottom left of footer' );
+bb_create_widget( 'Middle Footer', 'footer_middle', 'Displays in the middle of the footer' );
+bb_create_widget( 'Right Footer', 'footer_right', 'Displays in the bottom right of the footer' );
